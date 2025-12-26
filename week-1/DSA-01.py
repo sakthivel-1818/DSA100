6fe23d9255fc4d19ad5694f9ed375ca91e361883
sakthi-1818 <sakthisurya297@gmail.com>
2025-12-26 20:15:26 +0530

    pattern print end

---
+++ week-1/DSA-01.py
@@ -0,0 +1,68 @@
+# n=4
+# for i in range(1,n+1):
+#     for j in range(1,n+2-i):
+#         print("*",end="")
+#     print()
+# n=4
+# for i in range(n,0,-1):
+#     for j in range(i,0,-1):
+#         print(j,end="")
+#     print()
+#
+# n=4
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print("*",end="")
+#     print()
+# n=4
+# for i in range(1,n+1):
+#     print(i*" *")
+# for j in range (n-1,0,-1):
+#         print(j*" *")
+# n=4
+# for row in range(1,n+1):
+#     for i in range(n-row):
+#         print(" ",end="")
+#     for j in range (row*2-1):
+#         print("*",end="")
+#     print()
+# n=4
+# for i in range (1,n+1):
+#     for j in range(n-i):
+#         print(" ",end="")
+#     for j in range(2*i-1):
+#         print("*",end="")
+#     print()
+# n=4
+# for row in range(n,0,-1):
+#     star=row*2-1
+#     space=n-row
+#     print(" "*space+"*"*star)
+#
+# n=4
+# for i in range (1,n+1):
+#     for j in range(n-i):
+#         print(" ",end="")
+#     for j in range(2*i-1):
+#         print("*",end="")
+#     print()
+#
+# for row in range(n-1,0,-1):
+#     star=row*2-1
+#     space=n-row
+#     print(" "*space+"*"*star)
+# n=4
+# count = 0
+# for i in range(1,n):
+#     for j in range(1,i+1):
+#         count+=1
+#         print(count,end="")
+#     print()
+
+# n=4
+# count = 0
+# for i in range(1,n):
+#     for j in range(1,i+1):
+#         count=i
+#         print(count,end="")
+#     print()
--- week-1/pattern  print-07.py
+++ week-1/pattern  print-07.py
@@ -24,8 +24,8 @@
 # n=3
 # for i in range (1,n+1):
 #     print(i*"*","",end="")
-n=4
-for i in range(1,n+1):
-    for j in range(1,n+2-i):
-        print(j,end="")
-    print()
+# n=4
+# for i in range(1,n+1):
+#     for j in range(1,n+2-i):
+#         print(j,end="")
+#     print()
