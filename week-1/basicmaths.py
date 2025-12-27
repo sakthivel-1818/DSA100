d2f2880acdd46cb3ef61156097b6da1ddd240ffb
sakthi-1818 <sakthisurya297@gmail.com>
2025-12-27 15:19:51 +0530

    maths basic

---
+++ week-1/basicmaths.py
@@ -0,0 +1,55 @@
+# Reverse a number
+# n=5473929
+# while n>0:
+#     ld=n%10
+#     n=n//10
+#     print(ld,end="")
+
+
+#first digit
+# n=5473929
+# while n>0:
+#     ld=n%10
+#     n=n//10
+#     if n==0:
+#         print(ld)
+
+#last digit
+# n=5473929
+# print(n%10)
+
+# count  the number in n
+# n=9878
+# count=0
+# while n>0:
+#     ld=n%10
+#     n=n//10
+#     count+=1
+# print(count)
+
+# sum of first and last digits
+# n=1
+# ld=n%10
+# total=ld
+# count=0
+# while n>0:
+#     fd=n%10
+#     if n==fd and count>0:
+#        total+=fd
+#     count+=1
+#     n=n//10
+# print(total)
+
+# a=3
+# b=3
+# k=1
+# count=1
+# n=int(a**b)
+# while n>0:
+#     ld=n%10
+#     if count==k:
+#         print(ld)
+#     count+=1
+#     n=n//10
+
+
