7846adaa3063b373d7d67173942503b8e0e8a311
sakthi-1818 <sakthisurya297@gmail.com>
2026-01-07 21:28:43 +0530

    recursion

---
+++ week-03/recursion/revdig in rec.py
@@ -0,0 +1,31 @@
+
+###### reverse and linear count of a number
+
+# def func(n):
+#     if n==0:
+#         return
+#     print(n,end=" ")
+#     func(n-1)
+# def main():
+#     func(5)
+# main()
+#
+# def printNos(n):
+#     if n == 0:
+#         return 1
+#
+#     printNos(n - 1)
+#     print(n)
+# printNos(10)
+
+# class Solution:
+#     def k(self, a):
+#         if a == 0:
+#             return
+#         print(a, end=" ")
+#         self.k(a-1)
+#
+#     def printNos(self, n):
+#         self.k(n)
+# obj=Solution()
+# obj.printNos(10)
